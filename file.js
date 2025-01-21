616b6e9e16e9666138f1769fdd0e157e9d425f44
divaynshu shekhar jha <divyanshu3636@gmail.com>
2025-01-21 22:10:35 +0530

    file.js

---
+++ file.js
@@ -0,0 +1,29 @@
+
+// file system pe system bithana sikhenge
+// writeing in Files
+// reading from Files
+// appending in files
+
+
+
+
+const fs= require('fs');
+
+// fs.writeFileSync("./test.txt" , "hey there"); //synchronus
+
+// fs.writeFile("./test.txt" , "hey there asynchonus",(err)=> {}); //asynchronus
+
+// const result=fs.readFileSync("./fileUnderstanding.txt", "utf-8");
+// console.log(result);  //synchronus read returns the result but asynchronus read don't and it requires a call-backfunction
+
+
+const result= fs.readFile("./fileUnderstanding.txt", "utf-8",(err,result)=>{
+    if(err)
+    {
+        console.log("error encountered");
+    }
+    else{
+        console.log(result);
+    }
+
+});
