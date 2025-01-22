295cddc0241182746e14c2c47ea3e3106ecb1f65
divaynshu shekhar jha <divyanshu3636@gmail.com>
2025-01-22 10:46:41 +0530

    basics-done

--- file.js
+++ file.js
@@ -9,21 +9,22 @@
 
 const fs= require('fs');
 
-// fs.writeFileSync("./test.txt" , "hey there"); //synchronus
+fs.writeFileSync("./test.txt" , "hey there"); //synchronus it is blocking 
 
-// fs.writeFile("./test.txt" , "hey there asynchonus",(err)=> {}); //asynchronus
+fs.writeFile("./test.txt" , "hey there asynchonus",(err)=> {}); //asynchronus
 
 // const result=fs.readFileSync("./fileUnderstanding.txt", "utf-8");
 // console.log(result);  //synchronus read returns the result but asynchronus read don't and it requires a call-backfunction
 
+// console.log(os.cpus().length);
 
-const result= fs.readFile("./fileUnderstanding.txt", "utf-8",(err,result)=>{
-    if(err)
-    {
-        console.log("error encountered");
-    }
-    else{
-        console.log(result);
-    }
+// const result= fs.readFile("./fileUnderstanding.txt", "utf-8",(err,result)=>{
+//     if(err)
+//     {
+//         console.log("error encountered");
+//     }
+//     else{
+//         console.log(result);
+//     }
 
-});
+// });
